57dfc123a80a7b197694386bffb003fb545c716f
Hubert Ruta <hubert202@o2.pl>
2023-08-28 12:24:15 +0200

    Connected to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to database!");
+}).catch((err) => {
+    console.log(err);
+});
 const app = express();
 app.listen(3000, () => {
     console.log("Server on port 3000!");
