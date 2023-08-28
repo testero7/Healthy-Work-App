5fd276e37820604797d370dc4b5979acbef39f47
Hubert Ruta <hubert202@o2.pl>
2023-08-28 12:48:37 +0200

    Created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            requred: true,
+            unique: true,
+            
+        },
+        email:{
+            type: String,
+            requred: true,
+            unique: true,
+        },
+        password:{
+            type: String,
+            requred: true
+        }
+    },
+ {timestamps: true});
+
+ const User = mongoose.model('User', userSchema);
+
+ export default User;
