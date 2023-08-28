f57e99fffb5835ae5b7c058d71215624fa4490f2
Hubert Ruta <hubert202@o2.pl>
2023-08-28 11:17:29 +0200

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React from 'react';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+
+    </Routes>
+    
+    </BrowserRouter>
+   
   )
 }
